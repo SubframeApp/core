77f47ce5451b2722a043fc57d782437579fe428d
Christoph Kolb <chris@uninspired.studio>
2024-11-22 16:23:48 +0100

    Externalize dependencies
    Also switch from rollup-plugin-typescript2 to rollup-plugin-esbuild, split up tsconfig files for each build, fix unexistant prop on select, add exports to package.json, set sideEffects to false and build typescript declarations with tsc

--- rollup.config.js
+++ rollup.config.js
@@ -1,56 +1,60 @@
-import commonjs from "@rollup/plugin-commonjs"
-import resolve from "@rollup/plugin-node-resolve"
-import peerDepsExternal from "rollup-plugin-peer-deps-external"
-import postcss from "rollup-plugin-postcss"
-import preserveDirectives from "rollup-plugin-preserve-directives"
-import typescript from "rollup-plugin-typescript2"
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import preserveDirectives from "rollup-plugin-preserve-directives";
+import esbuild from "rollup-plugin-esbuild";
 
 /** @type {import("rollup").InputOptions} */
-
 const sharedConfig = {
   sourcemap: true,
   preserveModules: true,
-  // taken from https://github.com/rollup/rollup/issues/3684#issuecomment-1535836196
-  // ensures that node_modules is properly preserved / bundled
-  preserveModulesRoot: "src",
-  entryFileNames: (chunkInfo) => {
-    if (chunkInfo.name.includes("node_modules")) {
-      return chunkInfo.name.replace(/node_modules/g, "external") + ".js"
-    }
+  preserveModulesRoot: "./src",
+};
 
-    return "[name].js"
-  },
-}
-
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      dir: "dist/cjs",
-      format: "cjs",
-      ...sharedConfig,
+export default [
+  {
+    input: "src/index.ts",
+    external: [/node_modules/],
+    plugins: [
+      peerDepsExternal(),
+      postcss({
+        modules: true,
+        inject: { insertAt: "top" },
+      }),
+      esbuild({
+        tsconfig: "tsconfig.json",
+      }),
+      preserveDirectives(),
+    ],
+    onwarn(warning, warn) {
+      if (
+        warning.code === "MODULE_LEVEL_DIRECTIVE" &&
+        warning.message.includes(`use client`)
+      ) {
+        return;
+      }
+      warn(warning);
     },
-    {
-      dir: "dist/esm",
-      format: "esm",
-      ...sharedConfig,
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    postcss({
-      modules: true,
-      inject: { insertAt: "top" },
-    }),
-    commonjs(),
-    typescript({ tsconfig: "./tsconfig.json" }),
-    preserveDirectives.default(),
-  ],
-  onwarn(warning, warn) {
-    if (warning.code === "MODULE_LEVEL_DIRECTIVE" && warning.message.includes(`use client`)) {
-      return
-    }
-    warn(warning)
+    output: [
+      {
+        dir: "dist/cjs",
+        format: "cjs",
+        ...sharedConfig,
+        plugins: [
+          esbuild({
+            tsconfig: "tsconfig.cjs.json",
+          }),
+        ],
+      },
+      {
+        dir: "dist/esm",
+        format: "es",
+        ...sharedConfig,
+        plugins: [
+          esbuild({
+            tsconfig: "tsconfig.esm.json",
+          }),
+        ],
+      },
+    ],
   },
-}
+];
--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,90 +1,121 @@
-"use client"
+"use client";
 
-import * as RadixSelect from "@radix-ui/react-select"
-import classNames from "classnames"
-import React from "react"
-import styles from "./select.module.css"
+import * as RadixSelect from "@radix-ui/react-select";
+import classNames from "classnames";
+import React from "react";
+import styles from "./select.module.css";
 
 interface RootProps extends RadixSelect.SelectProps {}
 
-export const Root = React.forwardRef<HTMLDivElement, RootProps>(function SelectRoot({ children, ...otherProps }, _ref) {
-  return <RadixSelect.Root {...otherProps}>{children}</RadixSelect.Root>
-})
+export const Root = React.forwardRef<HTMLDivElement, RootProps>(
+  function SelectRoot({ children, ...otherProps }, _ref) {
+    return <RadixSelect.Root {...otherProps}>{children}</RadixSelect.Root>;
+  }
+);
 
-export const Trigger = React.forwardRef<HTMLButtonElement, RadixSelect.SelectTriggerProps>(function Trigger(
-  { className, children, placeholder, ...otherProps },
-  ref,
-) {
+export const Trigger = React.forwardRef<
+  HTMLButtonElement,
+  RadixSelect.SelectTriggerProps
+>(function Trigger({ className, children, ...otherProps }, ref) {
   return (
-    <RadixSelect.Trigger className={classNames(className, styles.trigger)} ref={ref} {...otherProps}>
+    <RadixSelect.Trigger
+      className={classNames(className, styles.trigger)}
+      ref={ref}
+      {...otherProps}
+    >
       {children}
     </RadixSelect.Trigger>
-  )
-})
+  );
+});
 
-export const Value = React.forwardRef<HTMLButtonElement, RadixSelect.SelectValueProps>(function Value(
+export const Value = React.forwardRef<
+  HTMLButtonElement,
+  RadixSelect.SelectValueProps
+>(function Value(
   {
     children, // NOTE: ignored
     placeholder,
     className,
     ...otherProps
   },
-  ref,
+  ref
 ) {
   return (
     // NOTE: this needs a wrapper since Radix strips all styling from RadixSelect.Value
-    <span className={classNames(className, styles.value)} {...otherProps} ref={ref}>
+    <span
+      className={classNames(className, styles.value)}
+      {...otherProps}
+      ref={ref}
+    >
       <RadixSelect.Value placeholder={placeholder} />
     </span>
-  )
-})
+  );
+});
 
 interface ContentProps extends RadixSelect.SelectContentProps {
-  portalClassName?: string
+  portalClassName?: string;
 }
 
-export const Content = React.forwardRef<HTMLDivElement, ContentProps>(function Content(
-  { className, children, portalClassName, position = "popper", sideOffset = 4, ...otherProps },
-  ref,
-) {
-  return (
-    <RadixSelect.Portal className={portalClassName}>
-      <RadixSelect.Content
-        className={classNames(className, styles.content)}
-        ref={ref}
-        position={position}
-        sideOffset={sideOffset}
-        {...otherProps}
-      >
-        <Select.Viewport asChild>{children}</Select.Viewport>
-      </RadixSelect.Content>
-    </RadixSelect.Portal>
-  )
-})
+export const Content = React.forwardRef<HTMLDivElement, ContentProps>(
+  function Content(
+    {
+      className,
+      children,
+      portalClassName,
+      position = "popper",
+      sideOffset = 4,
+      ...otherProps
+    },
+    ref
+  ) {
+    return (
+      <RadixSelect.Portal className={portalClassName}>
+        <RadixSelect.Content
+          className={classNames(className, styles.content)}
+          ref={ref}
+          position={position}
+          sideOffset={sideOffset}
+          {...otherProps}
+        >
+          <Select.Viewport asChild>{children}</Select.Viewport>
+        </RadixSelect.Content>
+      </RadixSelect.Portal>
+    );
+  }
+);
 
-export const Viewport = React.forwardRef<HTMLDivElement, RadixSelect.SelectViewportProps>(function Viewport(
-  { className, children, ...otherProps },
-  ref,
-) {
+export const Viewport = React.forwardRef<
+  HTMLDivElement,
+  RadixSelect.SelectViewportProps
+>(function Viewport({ className, children, ...otherProps }, ref) {
   return (
-    <RadixSelect.Viewport className={classNames(className, styles.viewport)} ref={ref} {...otherProps}>
+    <RadixSelect.Viewport
+      className={classNames(className, styles.viewport)}
+      ref={ref}
+      {...otherProps}
+    >
       {children}
     </RadixSelect.Viewport>
-  )
-})
+  );
+});
 
-export const Item = React.forwardRef<HTMLDivElement, RadixSelect.SelectItemProps>(function Item(
-  { className, children, value, ...otherProps },
-  ref,
-) {
+export const Item = React.forwardRef<
+  HTMLDivElement,
+  RadixSelect.SelectItemProps
+>(function Item({ className, children, value, ...otherProps }, ref) {
   return (
-    <RadixSelect.Item className={classNames(className, styles.item)} ref={ref} value={value} {...otherProps}>
+    <RadixSelect.Item
+      className={classNames(className, styles.item)}
+      ref={ref}
+      value={value}
+      {...otherProps}
+    >
       {children}
     </RadixSelect.Item>
-  )
-})
+  );
+});
 
-export const ItemText = RadixSelect.ItemText
+export const ItemText = RadixSelect.ItemText;
 
 export const Select = {
   Root,
@@ -94,4 +125,4 @@ export const Select = {
   Viewport,
   Item,
   ItemText,
-}
+};
