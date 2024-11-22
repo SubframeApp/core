e69c688b1e467ea504b4fc3fe56479c85ffe2e8c
Christoph Kolb <chris@uninspired.studio>
2024-11-22 17:36:38 +0100

    Correctly set patches, set external in rollup config correctly, fix collapsible type error

--- src/components/collapsible.tsx
+++ src/components/collapsible.tsx
@@ -1,46 +1,59 @@
-"use client"
+"use client";
 
-import * as RadixCollapsible from "@radix-ui/react-collapsible"
-import { Slot, SlotProps } from "@radix-ui/react-slot"
-import classNames from "classnames"
-import React from "react"
-import styles from "./collapsible.module.css"
+import * as RadixCollapsible from "@radix-ui/react-collapsible";
+import { Slot, SlotProps } from "@radix-ui/react-slot";
+import classNames from "classnames";
+import React from "react";
+import styles from "./collapsible.module.css";
 
 interface ChevronProps extends SlotProps {}
-export const Chevron = React.forwardRef<HTMLElement, ChevronProps>(function CollapsibleChevron(
-  { className, ...otherProps }: ChevronProps,
-  ref,
-) {
-  return <Slot ref={ref} className={classNames(className, styles.chevron)} {...otherProps} />
-})
+export const Chevron = React.forwardRef<HTMLElement, ChevronProps>(
+  function CollapsibleChevron({ className, ...otherProps }: ChevronProps, ref) {
+    return (
+      <Slot
+        ref={ref}
+        className={classNames(className, styles.chevron)}
+        {...otherProps}
+      />
+    );
+  }
+);
 
-export const Trigger = React.forwardRef<HTMLButtonElement, RadixCollapsible.CollapsibleTriggerProps>(function Trigger(
-  { className, children, placeholder, ...otherProps },
-  ref,
-) {
+export const Trigger = React.forwardRef<
+  HTMLButtonElement,
+  RadixCollapsible.CollapsibleTriggerProps
+>(function Trigger({ className, children, ...otherProps }, ref) {
   return (
-    <RadixCollapsible.Trigger className={classNames(className, styles.trigger)} ref={ref} {...otherProps}>
+    <RadixCollapsible.Trigger
+      className={classNames(className, styles.trigger)}
+      ref={ref}
+      {...otherProps}
+    >
       {children}
     </RadixCollapsible.Trigger>
-  )
-})
+  );
+});
 
-export const Content = React.forwardRef<HTMLDivElement, RadixCollapsible.CollapsibleContentProps>(function Content(
-  { className, children, ...otherProps },
-  ref,
-) {
+export const Content = React.forwardRef<
+  HTMLDivElement,
+  RadixCollapsible.CollapsibleContentProps
+>(function Content({ className, children, ...otherProps }, ref) {
   return (
-    <RadixCollapsible.Content className={classNames(className, styles.content)} ref={ref} {...otherProps}>
+    <RadixCollapsible.Content
+      className={classNames(className, styles.content)}
+      ref={ref}
+      {...otherProps}
+    >
       {children}
     </RadixCollapsible.Content>
-  )
-})
+  );
+});
 
-export const Root = RadixCollapsible.Root
+export const Root = RadixCollapsible.Root;
 
 export const Collapsible = {
   Root,
   Trigger,
   Content,
   Chevron,
-}
+};
